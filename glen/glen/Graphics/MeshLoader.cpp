94b98a52c13006d810d3583e5e46ddf5277a1610
Sonaza <temu92@gmail.com>
2013-02-14 12:54:07 +0200

    Update glen/glen/Graphics/MeshLoader.cpp

--- glen/glen/Graphics/MeshLoader.cpp
+++ glen/glen/Graphics/MeshLoader.cpp
@@ -12,27 +12,6 @@
 namespace
 {
 	//////////////////////////////////////////////////////////////
-	std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems)
-	{
-		std::stringstream ss(s);
-		std::string item;
-
-		while(std::getline(ss, item, delim))
-		{
-			elems.push_back(item);
-		}
-
-		return elems;
-	}
-
-	//////////////////////////////////////////////////////////////
-	std::vector<std::string> split(const std::string &s, char delim)
-	{
-		std::vector<std::string> elems;
-		return split(s, delim, elems);
-	}
-
-	//////////////////////////////////////////////////////////////
 	template <typename T>
 	T strto(const std::string& s)
 	{
